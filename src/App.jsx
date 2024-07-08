d143f2e46c2acc0774142c8745729bbdddf9e488
mattismyname3011 <mattismyname3011@gmail.com>
2024-07-08 18:46:26 +0800

    4. Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Mattismyname" body="React.js is awesome!" />
+      <Post author="Fyrefly" body="Check out on my instagram!" />
+    </main>
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,9 @@
-const names = ["Mattismyname", "Fyrefly"];
-
-function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+/* eslint-disable react/prop-types */
+function Post(props) {
   return (
     <div>
-      {/* <p>Mattismyname</p>
-      <p>React.js is awesome!</p> */}
-      <p>{chosenName}</p>
-      <p>React.js is awesome!</p>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
     </div>
   );
 }
